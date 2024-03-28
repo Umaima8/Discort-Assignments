7cbd4923c62baa9ec39fa068e68b83edae55deac
UmaimaFareed <umaimafaird88@gmail.com>
2024-03-28 21:39:34 +0500

    added Assignment 50

---
+++ Assignment 50/literals.js
@@ -0,0 +1,7 @@
+//template literals are something like this '${}'
+var morning = "early in the morning";
+var mustInclude = "excersises";
+var work = "pursuing meaningful work";
+var end = "spending quality time with loved ones";
+var ideal_day = "\n1)\"My ideal day starts with waking up ".concat(morning, ".\n2)It must include all the workout and ").concat(mustInclude, ".\n3)As time goes by i would be ").concat(work, " and it ends with ").concat(end, "\".");
+console.log(ideal_day);
---
+++ Assignment 50/literals.ts
@@ -0,0 +1,16 @@
+//template literals are something like this '${}'
+
+let morning: string = "early in the morning"
+
+let mustInclude: string = "excersises"
+
+let work: string = "pursuing meaningful work"
+
+let end: string = "spending quality time with loved ones"
+
+const ideal_day: string = `
+1)My ideal day starts with waking up ${morning}.
+2)It must include all the workout and ${mustInclude}.
+3)As time goes by i would be ${work} and it ends with ${end}.`
+
+console.log(ideal_day)
