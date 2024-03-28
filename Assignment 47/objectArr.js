648783b6bcafba08fe532dea8fa9e914be4c59b7
UmaimaFareed <umaimafaird88@gmail.com>
2024-03-28 21:35:19 +0500

    added Assignment 47

---
+++ Assignment 47/objectArr.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var laptops = [
+    { make: "Lenovo", model: "XS10", year: 2018 },
+    { make: "Apple", model: "MacBOOK-13", year: 2023 },
+    { make: "HP", model: "y76", yearno: 2019 }
+];
+var laptop1 = laptops[0], laptop2 = laptops[1];
+console.log(laptop1);
+console.log(laptop2);
---
+++ Assignment 47/objectArr.ts
@@ -0,0 +1,29 @@
+
+
+let laptops: object[] = [
+
+    {make: "Lenovo", model:  "XS10", year:2018},
+
+    {make: "Apple" , model: "MacBOOK-13", year: 2023},
+
+    {make: "HP", model: "y76", yearno:2019}
+
+]
+
+let [laptop1, laptop2] = laptops
+
+
+
+
+console.log(laptop1)
+
+console.log(laptop2)
+
+
+
+
+export{}
+
+
+
+
