d5e48ff0e4366eabb06b91083ad651d8ac918574
UmaimaFareed <umaimafaird88@gmail.com>
2024-04-01 00:56:42 +0500

    added Assignment 51

---
+++ Assignment 51/Arrow.ts
@@ -0,0 +1,11 @@
+//area of rectangle
+
+let area = (length: number, width: number): number => {
+
+    return length * width
+
+}
+
+let rectangle = area(12,8)
+
+console.log(rectangle)
---
+++ Assignment 51/arrow.js
@@ -0,0 +1,6 @@
+//area of rectangle
+var area = function (length, width) {
+    return length * width;
+};
+var rectangle = area(12, 8);
+console.log(rectangle);
