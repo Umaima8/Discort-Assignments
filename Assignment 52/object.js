a3d4744c849212d8352af4788a15790cdeb9b5ac
UmaimaFareed <umaimafaird88@gmail.com>
2024-04-01 00:57:59 +0500

    added Assignment 52

---
+++ Assignment 52/object.js
@@ -0,0 +1,11 @@
+var smartphone = {
+    brand: "RedME",
+    model: "RedMe9pro",
+    storage: "64 MB",
+    screenSize: "6.53-inch",
+    specialty: {
+        battery: "18-hours",
+        camera: "64 Mega-pixel"
+    }
+};
+console.log(smartphone);
---
+++ Assignment 52/object.ts
@@ -0,0 +1,16 @@
+let smartphone = {
+
+    brand: "RedME",
+    model: "RedMe9pro",
+    storage: "64 MB",
+    screenSize: "6.53-inch",
+    specialty:  {
+
+        battery: "18-hours",
+        camera: "64 Mega-pixel"
+    }
+
+
+}
+
+console.log(smartphone)
