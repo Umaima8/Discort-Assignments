28f1628c01f75541fdee54d04fd0eeb725adc647
UmaimaFareed <umaimafaird88@gmail.com>
2024-04-01 00:59:58 +0500

    added Assignment 55

---
+++ Assignment 55/numbers.js
@@ -0,0 +1,7 @@
+var numbers = [2, 4, 6, 8, 10];
+var doubleNumbers = [];
+for (var i = 0; i < numbers.length; i++) {
+    var doubled = numbers[i] * 2;
+    doubleNumbers.push(doubled);
+}
+console.log(doubleNumbers);
---
+++ Assignment 55/numbers.ts
@@ -0,0 +1,13 @@
+let numbers: number[] = [2,4,6,8,10]
+
+let doubleNumbers: number[] = []
+
+for (let i= 0; i<numbers.length; i++) {
+
+    let doubled= numbers[i] * 2
+
+    doubleNumbers.push(doubled)
+
+}
+
+console.log(doubleNumbers)
