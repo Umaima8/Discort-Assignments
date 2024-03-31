730e6936ef1b4498598e2c5fceeda713c945d628
UmaimaFareed <umaimafaird88@gmail.com>
2024-04-01 01:12:10 +0500

    added Assignment 65

---
+++ Assignment 65/remainder.js
@@ -0,0 +1,8 @@
+function remainder(num1, num2) {
+    var remaiders = (num1 % num2);
+    return (remaiders);
+}
+var division1 = remainder(5, 2);
+console.log(division1);
+var division2 = remainder(6, 3);
+console.log(division2);
---
+++ Assignment 65/remainder.ts
@@ -0,0 +1,16 @@
+function remainder (num1: number, num2: number): number {
+
+    
+    let remaiders = (num1 % num2)
+
+    return (remaiders)
+
+}
+
+let division1 = remainder(5,2)
+
+console.log(division1)
+
+let division2 = remainder(6,3)
+
+console.log(division2)
