57ee7868aa42ae29d482f9c3c799814411c58fad
UmaimaFareed <umaimafaird88@gmail.com>
2024-04-01 01:11:45 +0500

    added Assignment 64

---
+++ Assignment 64/merging.js
@@ -0,0 +1,8 @@
+function merging(text, num) {
+    var merge = text + num;
+    return merge;
+}
+var merge1 = merging("Age: ", 18);
+var merge2 = merging("CNIC: ", 234567);
+console.log(merge1);
+console.log(merge2);
---
+++ Assignment 64/merging.ts
@@ -0,0 +1,15 @@
+function merging (text: string, num: number): string {
+
+    let merge = text + num
+    
+    return merge
+
+}
+
+let merge1 = merging("Age: ",18)
+
+let merge2 = merging("CNIC: ",234567)
+
+console.log(merge1)
+
+console.log(merge2)
