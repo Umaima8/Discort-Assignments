4b7cc3fec2dc1a66adad1e783e680048b61df4c9
UmaimaFareed <umaimafaird88@gmail.com>
2024-04-01 00:58:46 +0500

    added Assignment 53

---
+++ Assignment 53/lists.js
@@ -0,0 +1,11 @@
+var lists = {
+    programming_languages: ["Typescript", "Jvascript", "HTML"],
+    Tools: ["Sublime text", "Atom", "gitHub"],
+    frameworks: ["Next.js", "Ruby", "Python"]
+};
+var skill1 = lists.programming_languages[0];
+var skill2 = lists.Tools[2];
+var skill3 = lists.frameworks[0];
+console.log("Languages:", skill1);
+console.log("Tools:", skill2);
+console.log("frameworks:", skill3);
