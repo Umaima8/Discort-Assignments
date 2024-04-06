d9d7cfb86d4667a37cf9d7674c95504c14d0a759
UmaimaFareed <umaimafaird88@gmail.com>
2024-04-06 06:23:19 +0500

    added Assignment 75

---
+++ Assignment 75/operators.js
@@ -0,0 +1,16 @@
+var x = 10;
+//performing addition operation
+x += 10;
+console.log("Addition: ", x);
+//performing subraction operation
+x -= 6;
+console.log("Subraction:", x);
+//performing multiplication operation
+x *= 3;
+console.log("Multiplication:", x);
+//performing division operation
+x /= 2;
+console.log("Division:", x);
+//performing remainder operation
+x %= 3;
+console.log("Remainder:", x);
