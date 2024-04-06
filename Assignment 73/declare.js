cbbc9057135e3e3338dc091f9f1c08d02d04422c
UmaimaFareed <umaimafaird88@gmail.com>
2024-04-06 06:22:30 +0500

    added Assignment 73

---
+++ Assignment 73/declare.js
@@ -0,0 +1,9 @@
+function declaration() {
+    //assigning a value
+    var val = 8;
+    console.log(val);
+    //reassigning the  value to the variable 'val'
+    val = 6;
+    console.log(val);
+}
+declaration();
---
+++ Assignment 73/declare.ts
@@ -0,0 +1,13 @@
+function declaration () {
+    
+    //assigning a value
+    let val: number = 8
+    console.log(val)
+
+
+    //reassigning the  value to the variable 'val'
+    val = 6
+    console.log(val)
+}
+
+declaration()
