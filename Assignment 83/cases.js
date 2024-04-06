808a83bf5697ed91928ddec99337b14a3455fb1e
UmaimaFareed <umaimafaird88@gmail.com>
2024-04-06 06:26:58 +0500

    added Assignment 83

---
+++ Assignment 83/cases.js
@@ -0,0 +1,6 @@
+function cases(str) {
+    var upperCase = str.toUpperCase();
+    var lowerCase = str.toLowerCase();
+    console.log("upper case: ", upperCase + " lower case: ", lowerCase);
+}
+cases("JavaScript");
---
+++ Assignment 83/cases.ts
@@ -0,0 +1,11 @@
+function cases (str: string) {
+
+    
+    let upperCase = str.toUpperCase()
+
+    let lowerCase = str.toLowerCase()
+
+    console.log("upper case: ",upperCase +  " lower case: ", lowerCase)
+}
+
+cases("JavaScript")
