20673d21a8c72531b96f3b234b8a71a5c38ddbd7
UmaimaFareed <umaimafaird88@gmail.com>
2024-04-06 06:28:35 +0500

    added Assignment 87

---
+++ Assignment 87/substring.js
@@ -0,0 +1,4 @@
+function substrings(str) {
+    return str.substring(0, 5);
+}
+console.log(substrings("WaterMelon"));
---
+++ Assignment 87/substring.ts
@@ -0,0 +1,7 @@
+function substrings (str: string): string {
+
+    return str.substring(0,5)
+
+}
+
+console.log(substrings("WaterMelon"))
