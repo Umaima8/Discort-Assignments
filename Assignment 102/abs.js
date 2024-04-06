68c5f677dddec6fcff1b8fff47e43c7304873b07
UmaimaFareed <umaimafaird88@gmail.com>
2024-04-06 06:34:18 +0500

    added Assignment 102

---
+++ Assignment 102/abs.js
@@ -0,0 +1,4 @@
+var x = -9 + 10;
+var y = -2 + 10;
+console.log(Math.abs(x));
+console.log(Math.abs(y));
---
+++ Assignment 102/abs.ts
@@ -0,0 +1,7 @@
+let x: number = -9 + 10
+
+let y: number = -2 +10
+
+console.log(Math.abs(x))
+
+console.log(Math.abs(y))
