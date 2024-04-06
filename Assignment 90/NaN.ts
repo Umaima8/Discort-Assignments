15b9fd65c439868bfea34718494640ff3b128beb
UmaimaFareed <umaimafaird88@gmail.com>
2024-04-06 06:29:37 +0500

    added Assignment 90

---
+++ Assignment 90/NaN.ts
@@ -0,0 +1,9 @@
+function NotaNumber (val: any): boolean {
+
+    return isNaN(val)
+
+}
+
+console.log(NotaNumber(10))
+
+console.log(NotaNumber("Typescript"))
---
+++ Assignment 90/Nan.js
@@ -0,0 +1,5 @@
+function NotaNumber(val) {
+    return isNaN(val);
+}
+console.log(NotaNumber(10));
+console.log(NotaNumber("Typescript"));
