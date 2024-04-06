b252b33791acde231d278554085cf183e0c87fcc
UmaimaFareed <umaimafaird88@gmail.com>
2024-04-06 06:25:50 +0500

    added Assignment 80

---
+++ Assignment 80/object.js
@@ -0,0 +1,9 @@
+var car = {
+    make: "BMW",
+    model: "X1",
+    year: "2020",
+    color: "Blue"
+};
+car.color = "Black";
+car.year = "2021";
+console.log(car);
---
+++ Assignment 80/object.ts
@@ -0,0 +1,15 @@
+let car = {
+    make: "BMW",
+    model: "X1",
+    year: "2020",
+    color: "Blue"
+};
+
+car.color = "Black"; 
+
+
+car.year = "2021"; 
+
+
+console.log(car); 
+
