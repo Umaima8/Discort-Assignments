93e183fae9578773fc713ca6f787b066fc5bb192
UmaimaFareed <umaimafaird88@gmail.com>
2024-04-06 06:34:54 +0500

    added Assignment 103

---
+++ Assignment 103/randomBoolean.js
@@ -0,0 +1,5 @@
+function randomBoolean() {
+    var randomValue = Math.random();
+    return randomValue > 0.5;
+}
+console.log(randomBoolean());
---
+++ Assignment 103/randomBoolean.ts
@@ -0,0 +1,10 @@
+function randomBoolean (): boolean {
+
+    let randomValue = Math.random()
+
+    return randomValue > 0.5
+        
+
+}
+
+console.log(randomBoolean())
