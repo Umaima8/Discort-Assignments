d863b286ea54a7d744342fa6add081e5b987cd24
UmaimaFareed <umaimafaird88@gmail.com>
2024-04-06 06:29:15 +0500

    added Assignment 89

---
+++ Assignment 89/parseInt.js
@@ -0,0 +1,5 @@
+function conversion(val) {
+    return Math.round(parseFloat(val));
+}
+console.log(conversion("4321"));
+console.log(conversion("45.89"));
---
+++ Assignment 89/parseInt.ts
@@ -0,0 +1,9 @@
+function conversion (val: string): number {
+
+    return (parseFloat(val))
+
+}
+
+console.log(conversion("4321"))
+
+console.log(conversion("45.89"))
