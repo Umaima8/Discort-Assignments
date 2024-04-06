e049276498651a8e47c57fccb2d1e4f264b57e2d
UmaimaFareed <umaimafaird88@gmail.com>
2024-04-06 06:33:05 +0500

    added Assignment 98

---
+++ Assignment 98/Days.ts
@@ -0,0 +1,32 @@
+function daysUntillNewYear (): number {
+
+//creating a new variable in which storing the Date function
+
+    
+    const today = new Date()  
+    
+    
+//We set it to January 1st of the next year by adding 1 to the current year (e.g., if today is in 2024, 
+//newYear will be January 1, 2025).The 0 represents 'January' and 1 represents 1st date of january
+    
+    
+    const newyear = new Date(today.getFullYear() + 1, 0, 1) 
+    
+
+//We subtract the timestamp of today from the timestamp of newYear in milliseconds.
+   
+    
+
+    const diff = newyear.getTime() - today.getTime() 
+    
+    
+   
+    const days = Math.ceil(diff/ (1000 /*to convert from milliseconds to seconds*/ * 60 /*to convert from seconds to minutes */ 
+    * 60 /* to convert from minutes to hours*/ * 24 /*to convert from hours to days*/ ))//Math.ceil is used to convert to whole numbers so even partial days are counted
+    
+    
+    return days
+
+}
+
+console.log(daysUntillNewYear() + " days untill New Year")
