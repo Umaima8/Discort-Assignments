65ae93f331d1474fcc305a0a60714d0558a7213c
UmaimaFareed <umaimafaird88@gmail.com>
2024-04-06 06:28:15 +0500

    added Assignment 86

---
+++ Assignment 86/includes.js
@@ -0,0 +1,5 @@
+function include(str) {
+    return str.includes("Javascript");
+}
+console.log(include("Javascript is the most flexible and fun  programming language to learn"));
+export {};
---
+++ Assignment 86/includes.ts
@@ -0,0 +1,7 @@
+function include (str: string): boolean {
+
+    return str.includes("Javascript")
+
+}
+
+console.log(include("Javascript is the most flexible and fun  programming language to learn"))
